bc18a1e6e60a017bcc5556c908e62b25737713d3
Joseph <jsdilorenzo730@gmail.com>
2022-09-21 12:03:07 -0400

    added a second python test file in test branch

---
+++ testpython2.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+print('Hello')
+
+if True:
+    print('truuuuuu')
+
+for i in range(1,10):
+    print(f'Thing number {i}')
+
+def useless_thing():
+    '''Does not actually do anything lol'''
+    pass
